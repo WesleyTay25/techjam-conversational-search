ad53f688666ed5f02a99081bfa08382a08cdf639
omiiho <honaomi470@gmail.com>
2026-09-01 00:41:22 +0800

    added boilerplate for llm reranker and tools

---
+++ tools/ablation_test.py
@@ -0,0 +1,7 @@
+def run_ablation():
+    # TODO: This script will eventually run the pipeline with and without the intent switch
+    print("Running Aunty Confirm ablation test...")
+    # We will track MTTC (Mean Time To Complete) here once LEAD wires the pipeline
+    
+if __name__ == "__main__":
+    run_ablation()
